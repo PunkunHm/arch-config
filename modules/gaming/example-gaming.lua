a3cab15fd6740b7e3fa514ece076097298d40ed6
PunkunHm <punprommit@gmail.com>
2026-01-22 00:31:23 +0900

    Update example-gaming.lua

--- modules/gaming/example-gaming.lua
+++ modules/gaming/example-gaming.lua
@@ -1,29 +1,29 @@
--- -- Gaming module example
--- -- Delete or customize this file
+-- Gaming module example
+-- Delete or customize this file
 
--- local packages = {
---     "steam",
---     "lutris",
---     "gamemode",
---     "lib32-gamemode",
---     "mangohud",
---     "lib32-mangohud",
--- }
+local packages = {
+    "steam",
+    "lutris",
+    "gamemode",
+    "lib32-gamemode",
+    "mangohud",
+    "lib32-mangohud",
+}
 
--- -- Add Wine for non-native games
--- table.insert(packages, "wine")
--- table.insert(packages, "wine-mono")
--- table.insert(packages, "winetricks")
+-- Add Wine for non-native games
+table.insert(packages, "wine")
+table.insert(packages, "wine-mono")
+table.insert(packages, "winetricks")
 
--- -- Add Discord via Flatpak
--- table.insert(packages, "flatpak:com.discordapp.Discord")
+-- Add Discord via Flatpak
+table.insert(packages, "flatpak:com.discordapp.Discord")
 
--- return {
---     description = "Gaming packages",
---     packages = packages,
---     conflicts = { "minimal" },
+return {
+    description = "Gaming packages",
+    packages = packages,
+    conflicts = { "minimal" },
 
---     services = {
---         enabled = { "gamemode.service" },
---     },
--- }
+    services = {
+        enabled = { "gamemode.service" },
+    },
+}
