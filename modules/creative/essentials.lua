8699504f722afe53a2d8817959b6d3520e6ca190
PunkunHm <punprommit@gmail.com>
2026-02-15 21:32:27 +0900

    Revamped creative module

---
+++ modules/creative/essentials.lua
@@ -0,0 +1,16 @@
+-- Essentials
+-- Must-have packages for creative tasks
+
+local packages = {
+    "flatpak:com.usebottles.bottles",
+    "winboat-bin", -- for windows apps including
+    "kdotool",     -- tuxbox dependency
+}
+
+return {
+    description = "Essentials/must-have packages for creative tasks",
+    packages = packages,
+    post_install_hook = "scripts/setup-tuxbox.sh",
+    hook_behavior = "ask",
+}
+-- Find a way to install hytale and apply decky + unifideck with steam
---
+++ modules/creative/scripts/setup-tuxbox.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd /opt/
+git clone https://github.com/AndyCappDev/tuxbox.git
+cd tuxbox
+./install.sh
